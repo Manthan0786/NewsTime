2d8c8482718271764a3bf8788dc4d04537ee4726
Manthan Bhardwaj <95605134+Manthan0786@users.noreply.github.com>
2022-08-07 15:10:42 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import News from './components/news';
+import Navbar from './components/navBar';
+import { Routes } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
+import { useState } from 'react';
+
+function App() {
+  const [progress, setProgress] = useState(0);
+  return (
+    <>
+      <BrowserRouter>
+        <LoadingBar
+          progress={progress}
+          color='#f11946'
+          waitingTime={1000}
+          height={3}
+          onLoaderFinished={() => setProgress(0)}
+        />
+        <div className='nav-bar-div'>
+          <Navbar />
+        </div>
+        <div className='news_container_div'>
+          <Routes>
+            <Route path="/" element={<News setProgress={setProgress} category='general' />} />
+            <Route path="entertainment" element={<News setProgress={setProgress} key='entertainment' category='entertainment' />} />
+            <Route path="sports" element={<News setProgress={setProgress} key='sports' category='sports' />} />
+            <Route path="business" element={<News setProgress={setProgress} key='business' category='business' />} />
+            <Route path="science" element={<News setProgress={setProgress} key='science' category='science' />} />
+            <Route path="health" element={<News setProgress={setProgress} key='health' category='health' />} />
+            <Route path="technology" element={<News setProgress={setProgress} key='technology' category='technology' />} />
+          </Routes>
+
+        </div>
+      </BrowserRouter>
+
+    </>
+  );
+}
+
+export default App;
